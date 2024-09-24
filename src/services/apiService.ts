14278efd019a20cf9f21696505bea5c987ff3d47
samedayramps <ty@samedayramps.com>
2024-09-24 12:30:12 -0500

    Commit changes and push

--- src/services/apiService.ts
+++
@@ -1,9 +0,0 @@
-export const fetchSalesProcesses = async () => {
-  try {
-    const response = await api.get('/api/sales-processes');
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching sales processes:', error);
-    throw error;
-  }
-};
